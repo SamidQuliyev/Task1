77aa41bf5d37a4a4a301a4d967a0129f10294a83
SamidQuliyev <samidquliyev2003@gmail.com>
2022-04-12 00:37:21 +0400

    init

---
+++ TASK1 - Copy/TASK1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TASK1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            int ədəd = 51;
+            if (ədəd % 2 == 1)
+            {
+                Console.Write("Yes");
+            }
+            else
+            {
+                Console.Write("No");
+            }
+            #endregion
+        }
+    }
+}
